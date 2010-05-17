1718cea3a5b506e77fd627d5833af707d6c1065e
sgtcoolguy <cwilliams@aptana.com>
2010-05-17 14:58:22 -0700

    Add special command to create and deploy app in one command fro our deploy wizard

--- commands/app.rb
+++ commands/app.rb
@@ -38,3 +38,16 @@ command "Rename App" do |cmd|
     Ruble::Terminal.open("heroku rename '#{HerokuTools.shell_escape(new_name)}'")
   end
 end
+
+# Special command used to create and deploy under deploy wizard. Not surfaced in Ruble UI
+command "Create and Deploy App" do |cmd|
+cmd.scope = ['source.ruby', 'project.rails']
+  cmd.output = :discard
+  cmd.working_directory = :current_project
+  cmd.invoke do |context|
+    cmd = "create"
+    cmd << " #{ENV['HEROKU_APP_NAME']}" if ENV['HEROKU_APP_NAME']
+    cmd << "; git push heroku master"
+    HerokuCmd.run(cmd)
+  end
+end
