760b31c713d50de9ff81209b4dd05d2267ea2631
Allen Yeung <ayeung@aptana.com>
2010-06-03 18:56:46 -0700

    Fixed installing heroku gem for windows

--- lib/heroku_bundle_tools.rb
+++ lib/heroku_bundle_tools.rb
@@ -1,6 +1,8 @@
 require 'ruble'
 require 'ruble/ui'
 require 'escape'
+require 'ruble/terminal'
+require 'rbconfig'
 
 class CommandRunner
   def initialize(command)
@@ -39,28 +41,33 @@ end
 
 module HerokuCmd
   def self.gem_is_installed?(name)
-    `gem search #{name}` =~ /#{Regexp.escape(name)}/
+    if is_windows?
+      `gem search #{name}` =~ /#{Regexp.escape(name)}/
+    else
+      `/bin/sh -l -c "gem search{name}"` =~ /#{Regexp.escape(name) }/
+    end
+  end
+  
+  def self.is_windows?
+    RbConfig::CONFIG['target_os'] =~ /(win|w)32$/
   end
   
   def self.run(command)
     # FIXME Doesn't seem to find my heroku installed in rvm
     #heroku_path = "~/.rvm/gems/ruby-1.8.7-p249/bin/heroku"
-    heroku_path = "heroku"
-    command = "#{heroku_path} #{command}"
-    unless system("#{heroku_path} help")
-      # heroku gem is probably not installed, check to make sure
-      if gem_is_installed?('heroku')
-        Ruble::UI.alert(:error, "Heroku command failed", "The Heroku gem appears to be installed but the heroku command failed to run.")
-        return false
-      else
-        if Ruble::UI.request_confirmation(
-            :title => "Install Heroku gem?", 
-            :prompt => "The Heroku gem was not found on your system. Would you like to install the heroku gem now?",
-            :button1 => "Install Heroku gem")
-          command = "sudo gem install heroku && #{command}" # TODO We dont necessarily want to run under sudo!
+    command = "heroku #{command}"
+    unless gem_is_installed?('heroku')
+      if Ruble::UI.request_confirmation(
+          :title => "Install Heroku gem?", 
+          :prompt => "The Heroku gem was not found on your system. Would you like to install the heroku gem now?",
+          :button1 => "Install Heroku gem")
+        if !is_windows?  
+          command = "sudo gem install heroku && #{command}"
         else
-          return false
+          command = "gem install heroku && #{command}"
         end
+      else
+        return false
       end
     end
     
